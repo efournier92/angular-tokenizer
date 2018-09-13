6e0eeffaf7ef990063ef2b3182e7a309b98a9267
misigno <efournier92@gmail.com>
2018-09-12 20:56:28 -0400

    finish revisions

--- promoCodes.js
+++ promoCodes.js
@@ -1,19 +1,26 @@
-var app = angular.module('app', ['ngTagsInput']);
+var app = angular.module('promoCodes', ['ngTagsInput']);
 
-//angular.module('app', ['ngTagsInput'])
-app.controller('MainCtrl', function($scope, $http) {
+app.controller('promoCodesCtrl', function($scope, $http) {
   $scope.selectedPromoCodes = [];
   var allPromoCodesString = '[ {"vanityCode": "AA123"}, {"vanityCode": "BB123"}, {"vanityCode": "CC123"}, {"vanityCode": "DD123"}, {"vanityCode": "EE123"}, {"vanityCode": "FF123"}, {"vanityCode": "GG123"}, {"vanityCode": "HH123"}, {"vanityCode": "AA987"}, {"vanityCode": "BB987"}, {"vanityCode": "CC987"}, {"vanityCode": "DD987"}, {"vanityCode": "EE987"}, {"vanityCode": "FF987"}, {"vanityCode": "GG987"}, {"vanityCode": "HH987"} ]';
   $scope.allPromoCodes = angular.fromJson(allPromoCodesString);
 
-  $scope.searchCodes = function(query) {
+  $scope.searchCodes = function (query) {
     var search_term = query.toUpperCase();
     $scope.codes = [];
     angular.forEach($scope.allPromoCodes, function(code) {
       if (code.vanityCode.toUpperCase().indexOf(search_term) >= 0)
         $scope.codes.push(code);
     });
-
     return $scope.codes;
+  };
+
+  $scope.promoCodesChanged = function () {
+    window.promoCodes = $scope.selectedPromoCodes;
+    console.log('Changed', window.promoCodes);
+  };
+
+  $scope.saveCodes = function () {
+    console.log('Codes saved.');
   };
 });
